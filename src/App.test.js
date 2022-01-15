7346a0da4c3a055b63528e861777c33d5f192353
mamulya063 <mamulya063@gmail.com>
2022-01-15 14:40:49 +0530

    firstcommit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { cleanup, render, screen } from "@testing-library/react";
+import RawJson from "./Projects/FinalApp/components/header";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+
+
+
+
+
+cleanup()
+test('test to render the rawJson',()=>{
+    render(<RawJson></RawJson>);
+
+   screen.debug()
+})
